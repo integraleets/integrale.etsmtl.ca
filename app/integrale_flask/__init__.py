b96b895a4c1752d813268737249c50ec0bca12e4
Jean-Michel Coupal <jean-michel@coup.al>
2016-04-23 18:04:03 -0400

    user-mgmt

--- app/integrale_flask/__init__.py
+++ app/integrale_flask/__init__.py
@@ -9,8 +9,12 @@ app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = 'contact@example.com'
 app.config["MAIL_PASSWORD"] = 'your-password'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:allo1234@localhost/development'
  
 from routes import mail
 mail.init_app(app)
- 
+
+from models import db
+db.init_app(app)
+
 import integrale_flask.routes
--- app/integrale_flask/routes.py
+++ app/integrale_flask/routes.py
@@ -1,7 +1,8 @@
 from integrale_flask import app
-from flask import render_template, request, flash
-from forms import ContactForm
+from flask import render_template, request, flash, session, url_for, redirect
+from forms import ContactForm, SignupForm, SigninForm
 from flask.ext.mail import Message, Mail
+from models import db, User
  
 mail = Mail()
 
@@ -30,16 +31,75 @@ def contact():
   elif request.method == 'GET':
     return render_template('contact.html', form=form)
 
+@app.route('/profile')
+def profile():
+ 
+  if 'email' not in session:
+    return redirect(url_for('signin'))
+ 
+  user = User.query.filter_by(email = session['email']).first()
+ 
+  if user is None:
+    return redirect(url_for('signin'))
+  else:
+    return render_template('profile.html')
 
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+  form = SignupForm()
+ 
+  if 'email' in session:
+    return redirect(url_for('profile')) 
+   
+  if request.method == 'POST':
+    if form.validate() == False:
+      return render_template('signup.html', form=form)
+    else:
+      newuser = User(form.firstname.data, form.lastname.data, form.email.data, form.password.data)
+      db.session.add(newuser)
+      db.session.commit()
+       
+      session['email'] = newuser.email
+      return redirect(url_for('profile'))
+   
+  elif request.method == 'GET':
+    return render_template('signup.html', form=form)
+
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+  form = SigninForm()
+ 
+  if 'email' in session:
+    return redirect(url_for('profile')) 
+   
+  if request.method == 'POST':
+    if form.validate() == False:
+      return render_template('signin.html', form=form)
+    else:
+      session['email'] = form.email.data
+      return redirect(url_for('profile'))
+                 
+  elif request.method == 'GET':
+    return render_template('signin.html', form=form)
+
+
+@app.route('/signout')
+def signout():
+ 
+  if 'email' not in session:
+    return redirect(url_for('signin'))
+     
+  session.pop('email', None)
+  return redirect(url_for('index'))
 
 
 @app.route("/membre")
 def membre():
     return render_template('membre.html')
 
-@app.route("/login")
-def login():
-    return render_template('login.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
